3b90ed7ff7a399a982f601c552ce495a27d46c9c
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-03-06 12:54:34 +0300

    Добавьте файлы проекта.

---
+++ lab23/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab23
+{
+    internal class Program
+    {
+        static void Method1()
+        {
+            Console.WriteLine("Method1 Has started");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Method1 provides counter = {i}");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Method1 had finished");
+        }
+        static void Method2(int n)
+        {
+            Console.WriteLine("Method2 Has started");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Method2 provides counter = {i}");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine("Method2 had finished");
+        }
+        static int Method3()
+        {
+            Console.WriteLine("Method3 Has started");
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                S += i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Method3 had finished");
+            return (S);
+        }
+        static void Main(string[] args)
+        {
+            /*Action action = new Action(Method1);
+            Task task = new Task(action);
+            task.Start();
+
+            Task task1 = new Task(Method1);
+
+            Task task2 = new Task(delegate () { Method1(); });
+
+            Task task3 = new Task(() => Method1());
+
+            Task task4 = Task.Factory.StartNew(action);*/
+
+            /*Task task5 = Task.Run(() => Method1());
+            task5.Wait(); //блокирующая операция
+
+            Task task6 = Task.Run(() => Method2(100));
+            task6.Wait(); //блокирующая операция
+            //Method1(); - синхронное программирование
+            //Method2(100);*/
+
+            int r = Method1Async().Result;
+            Method2(100);
+            Console.WriteLine("Main had finished");
+            Console.ReadKey();
+
+        }
+        static async Task<int> Method1Async()
+        {
+            Console.WriteLine("Method1Async has started");
+            int result = await Task.Run(() => Method3());
+            Console.WriteLine("Method1Async had finished");
+            return result;
+           
+
+        }
+    }
+}
---
+++ lab23zadacha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+// Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.
+
+namespace lab23zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите число для определения его факториала:  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactAsync(n);
+
+            Console.ReadKey();
+        }
+
+        static void Fact(int n)// метод  - определение факториала
+        {
+            int s = 1;// факториал с 1
+            for (int i = 1; i < n + 1; i++)
+            {
+                s *= i;// произведение всех чисел
+                Thread.Sleep(50);
+            }
+            Console.WriteLine(s);
+        }
+        static async void FactAsync(int n)// асинхронный метод
+        {
+            await Task.Run(() => Fact(n));// задача с await
+        }
+    }
+}
